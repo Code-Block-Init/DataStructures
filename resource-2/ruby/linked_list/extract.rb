d9b0e9c91263613ebcac9fdbe09c95abb7ad54ca
Aswini S <ashumeow@live.com>
2015-03-09 02:00:05 +0530

    add extract

---
+++ resource-2/ruby/linked_list/extract.rb
@@ -0,0 +1,19 @@
+class linked_list
+	def extract(item)
+		ptr = @head
+		prevPtr = nil
+		while not ptr.nil? and not ptr.datum.equal?(item)
+			prevPtr = ptr
+			ptr = ptr.succ
+		end
+		raise ArgumentError if ptr.nil?
+		if ptr == @head
+			@head = ptr.succ
+		else
+			prevPtr.succ = ptr.succ
+		end
+		if ptr == @tail
+			@tail = prevPtr
+		end
+	end
+end
