579e92fedb22d1fe8de44f423cf5e5bd571b19d7
Aswini S <ashumeow@live.com>
2015-03-14 07:10:09 +0530

    add init and purge to stack as array

---
+++ resource-2/ruby/stack/array/init_purge.rb
@@ -0,0 +1,12 @@
+class ArrayStack < ArrayStack
+	def init(size = 0)
+		super()
+		@array = Array.new(size)
+	end
+	def purge
+		while @count > 0
+			@count -= 1
+			@array[@count] = nil
+		end
+	end
+end
