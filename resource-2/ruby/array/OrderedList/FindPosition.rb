753e1421bbf4496b81d4e6b64449c2ecd94f7549
Aswini S <ashumeow@live.com>
2015-03-31 04:01:19 +0530

    add finding position

---
+++ resource-2/ruby/array/OrderedList/FindPosition.rb
@@ -0,0 +1,14 @@
+class ArrayOrderedList < OrderedList 
+	# Finding the Position
+	def FindPosition(obj)
+		i = 0
+		while i < @count and not @array[i] == obj
+			i += 1
+		end
+		Cursor.new(self, i)
+	end
+	def [](offset)
+		raise IndexError if not (0 ... @count) === offset
+		@array[offset]
+	end
+end
