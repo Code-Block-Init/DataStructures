3c75bc42d692bfc1916aafc8e0e5d74d14357561
Aswini S <ashumeow@live.com>
2015-04-16 06:13:02 +0530

    add join

---
+++ resource-2/ruby/partition/join.rb
@@ -0,0 +1,8 @@
+class PartitionWorld < Partition 
+	def join(s,t)
+		assert { member?(s) and s.parent.nil? and
+			member?(t) and t.parent.nil? and not s.equal(t) }
+		t.parent = s
+		@count -= 1
+	end
+end
